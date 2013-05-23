497ce9775f22ac2945b10a50717a20053cab025d
Ryan Davis <ryand@zenspider.com>
2013-05-22 22:01:49 -0800

    Updated for minitest 5 [git-p4: depot-paths = "//src/seattlerb_dashboard/dev/": change = 8602]

--- test/test_seattlerb_dashboard.rb
+++ test/test_seattlerb_dashboard.rb
@@ -1,7 +1,7 @@
 require "minitest/autorun"
 require "seattlerb_dashboard"
 
-class TestSeattlerbDashboard < MiniTest::Unit::TestCase
+class TestSeattlerbDashboard < Minitest::Test
   def test_sanity
     # flunk "write tests or I will kneecap you"
   end
